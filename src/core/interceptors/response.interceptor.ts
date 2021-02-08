d97127e1e9dcb0d40cece3546d246583a1aa22de
Shipu Ahamed <shipuahamed01@gmail.com>
2021-02-08 23:34:36 +0600

    Update response.interceptor.ts

--- src/core/interceptors/response.interceptor.ts
+++ src/core/interceptors/response.interceptor.ts
@@ -1,4 +1,9 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -13,12 +18,29 @@ export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
     next: CallHandler,
   ): Observable<Response<T>> {
     const response = context.switchToHttp().getResponse();
+
     return next.handle().pipe(
-      map((data) => ({
-        status: response.statusCode,
-        message: response.statusMessage,
-        data: data,
-      })),
+      map((data) => {
+        let message = null;
+        let status = null;
+        if (data?.message) {
+          message = data.message;
+          delete data.message;
+        }
+
+        if (data?.statusCode) {
+          status = data.statusCode;
+          delete data.statusCode;
+        }
+
+        status = status || response.statusCode;
+        response.status(status);
+        return {
+          status: status,
+          message: message || response.statusMessage || 'OK',
+          data: data || {},
+        };
+      }),
     );
   }
 }
