11033dfe48a408d5bc73a3d2d7c6e460ab0b7a75
Shipu Ahamed <shipuahamed01@gmail.com>
2021-02-15 12:30:03 +0600

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { logger } from './common/middlewares/logger.middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization, SERVICE, X-EVALY-PLATFORM',
+  });
   app.use(logger);
   await app.listen(configService.get('APP_PORT'));
   console.log(
